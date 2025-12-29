83b79bfe5077eabfed111e147cb4be8439ec143d
Qerakl <german111474457@gmail.com>
2025-12-29 15:00:29 +0500

    Создал Enum класс для статусов задач, добавил отношения для задач и пользователей, добавил мягкое удаление

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,11 +2,28 @@
 
 namespace App\Models;
 
+use App\Enums\TaskStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Task extends Model
 {
-    /** @use HasFactory<\Database\Factories\TaskFactory> */
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+    ];
+
+    protected $casts = [
+        'status' => TaskStatus::class,
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/migrations/2025_12_29_093625_create_tasks_table.php
+++ database/migrations/2025_12_29_093625_create_tasks_table.php
@@ -1,25 +1,29 @@
 <?php
 
+use App\Enums\TaskStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->string('title');
+            $table->text('description')->nullable();
+
+            $table->enum('status', array_map(fn($c) => $c->value, TaskStatus::cases()))
+                ->default(TaskStatus::New->value)
+                ->index();
+
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('tasks');
